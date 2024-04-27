68a7b76cb3720ec11e1aa35b6eb8163642703e48
emarrugo <emarrugo@proteccion.com.co>
2024-04-26 22:43:42 -0500

    Initial commit

---
+++ src/test/java/com/emarrugor1/roundrobin/RoundRobinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emarrugor1.roundrobin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundRobinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
